44f9caecbd148e07f9e6c3c8e4ce72a907d83f2f
pli01 <pli01@users.noreply.github.com>
2024-02-09 00:20:54 +0100

    node_label label

--- k3s-ansible/inventory/local/01-dynamic-meta-data.sh
+++ k3s-ansible/inventory/local/01-dynamic-meta-data.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 #
 # quick dynamic inventory script
-#   assign host to ansible group define in meta-data tags "kube_groups"
-#   kube_groups = "group_a,group_b"
+#   assign host to ansible group defined in meta-data tags "kube_groups"
+#     kube_groups = "group_a,group_b"
+#   assign node_label_list defined in meta-data tags "k3s.label"
 #
 
 hostname="$(hostname -s)"
 KUBE_GROUPS="$(curl --retry 20 --retry-delay 5 -qs http://169.254.169.254/latest/meta-data/tags/kube_groups)"
+
+meta_data_url=http://169.254.169.254/latest/meta-data/tags
+meta_data_labels_key="$(curl -s $meta_data_url |grep k3s.label )"
+meta_data_labels="$(for label in $meta_data_labels_key ; do curl -s $meta_data_url/$label |xargs -i echo \"${label#k3s.label.}={}\" ; done)"
+NODE_LABELS="$(echo $meta_data_labels|sed -e 's/^/\[/;s/ /,/g;s/$/\]/')"
 
 if [ "$1" == "--list" ]; then
 cat<<EOF
@@ -18,7 +24,7 @@ echo "$KUBE_GROUPS" |tr ',' '\n' |while read group ; do
 EOF
 done
   cat <<EOF
-     "_meta": { "hostvars": { "${hostname}": {} } }
+     "_meta": { "hostvars": { "${hostname}": { "node_label_list": ${NODE_LABELS} } } }
 }
 EOF
 elif [ "$1" == "--host" ]; then
